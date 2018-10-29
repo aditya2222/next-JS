9f3490059588588309c5d098c061dbf223596d71
Aditya <adityasingh222247@gmail.com>
2018-10-29 18:58:46 +0530

    Next js components and pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import User from '../../components/user'
 
 const authIndexPage = (props) => {
     return (
         <div>
             <h1>The Auth Index Page</h1>
+            <User name="somename" age={28} />
             <p>Go to <Link href="/"><a>Home</a></Link></p>
         </div>
     )
