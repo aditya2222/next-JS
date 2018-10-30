97f294fc7f2c06f2014305442f0664e7f2bb1662
Aditya <adityasingh222247@gmail.com>
2018-10-30 11:14:04 +0530

    Next JS lifecycle hook

--- components/user.js
+++ components/user.js
@@ -7,16 +7,16 @@ const user = (props) => {
             <h1>{props.name}</h1>
             <p>{props.age}</p>
             <style jsx>
-            {`
+                {`
 
-     			div {
-     				border: 1px solid #eee;
-     				box-shadow: 0 2px 3px #ccc;
-     				padding: 20px;
-     				text-align: center;
-     			}       	
+                div {
+                    border: 1px solid #eee;
+                    box-shadow: 0 2px 3px #ccc;
+                    padding: 20px;
+                    text-align: center;
+                }       	
 
-            `}
+                `}
             </style>
         </div>
     )
